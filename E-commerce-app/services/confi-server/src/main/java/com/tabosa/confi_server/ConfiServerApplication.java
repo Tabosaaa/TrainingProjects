4a5e2fc33ce98eb01020a3500320d338eaa78f87
Tabosaaa <tabosalucas2011@gmail.com>
2025-03-07 02:13:54 -0300

    start e-commerce backend with microservices

---
+++ E-commerce-app/services/confi-server/src/main/java/com/tabosa/confi_server/ConfiServerApplication.java
@@ -0,0 +1,13 @@
+package com.tabosa.confi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiServerApplication.class, args);
+	}
+
+}
---
+++ E-commerce-app/services/confi-server/src/test/java/com/tabosa/confi_server/ConfiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabosa.confi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
