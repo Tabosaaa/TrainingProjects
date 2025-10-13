42d5aedf7057127ba73a6866639116457c1b6e32
Tabosaaa <tabosalucas2011@gmail.com>
2025-10-12 22:50:49 -0300

    Validating templates at startup

--- go-course/main.go
+++ go-course/main.go
@@ -1,60 +1,40 @@
 package main
 
-//class i must see: why do we use server side rendering?
 import (
-	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/tabosaaa/go-course/views"
 	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/tabosaaa/go-course/controllers"
+	"github.com/tabosaaa/go-course/views"
 )
-
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	tpl, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	tpl.Execute(w, nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := "templates/home.gohtml"
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := "templates/contact.gohtml"
-	executeTemplate(w, tplPath)
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := "templates/faq.gohtml"
-	executeTemplate(w, tplPath)
-}
-
-//func pathHandler(w http.ResponseWriter, r *http.Request) {
-//	switch r.URL.Path {
-//	case "/":
-//		homeHandler(w, r)
-//	case "/contact":
-//		contactHandler(w, r)
-//	case "/faq":
-//		faqHandler(w, r)
-//	default:
-//		http.Error(w, "Not Found", http.StatusNotFound)
-//	}
-//}
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Not Found", http.StatusNotFound)
+		http.Error(w, "Page not found", http.StatusNotFound)
 	})
-	fmt.Println("Server is running on port http://localhost:3000")
-	http.ListenAndServe(":3000", r)
+
+	log.Println("listening on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
